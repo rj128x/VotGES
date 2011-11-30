563b8d9b35b92d479cb336a210d87d423f4d6578
rj128x <rj128x@gmail.com>
2011-11-30 13:40:19 +0600

    123

--- RUSA/Program.cs
+++ RUSA/Program.cs
@@ -9,7 +9,12 @@ namespace RUSA
 {
 	class Program
 	{
+
+
+
+
 		static void Main(string[] args) {
+			
 			Logger.init(new ConsoleLogger());
 			//Console.WriteLine(RashodTable.getStationRashod(349, 13, RashodCalcMode.min));
 
@@ -19,27 +24,81 @@ namespace RUSA
 			}
 
 			List<double> napors=new List<double>();
-			for (double napor=16; napor <= 22; napor+=1) {
+			for (double napor=16; napor <= 21; napor+=1) {
 				napors.Add(napor);
 			}
-			calc(powers, napors, true, @"d:\RUSA_BY_POWER.html");
+
+			calcFull(powers, napors);
+			//calc(powers, napors, true, @"d:\RUSA_BY_POWER.html");
 			//calc(napors, powers, false, @"d:\RUSA_BY_NAPOR.html");
 
 			/*int[] avail= { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 			RUSADiffPower rusa=new RUSADiffPower(avail);*/
 
 			/*List<int> sostav=new List<int>();
-			double rashodEq = VotGES.Rashod.RUSA.getOptimRashod(250, 16, true,sostav);
+			double rashodEq = VotGES.Rashod.RUSA.getOptimRashod(300, 16, true,sostav);
 			sostav.Sort();
 			RUSADiffPower rusa=new RUSADiffPower();
-			double rashodDiff=rusa.getMinRashod(sostav,250, 16);
+			double rashodDiff=rusa.getMinRashod(sostav,300, 16);
 			Console.WriteLine(rashodDiff.ToString());
 			Console.WriteLine(rashodEq.ToString());
 			Console.WriteLine(String.Join("~", sostav));*/
 			
 		}
 
+		protected static void calcFull(List<double> powers, List<double> napors) {
+			List<int> allGa=new List<int>();
+			for (int ga=1; ga <= 10; ga++)
+				allGa.Add(ga);
+			List<int> sostav=new List<int>();
+			RUSADiffPowerNapors rusa=new RUSADiffPowerNapors();
+			double eqRashod=0;
+			double diffRashod=0;
+			double idealRashod=0;
+			double eq=0;
+			double diff=0;
+			double ideal=0;
+			double eqByX;
+			double diffByX;
+			double idealByX;
+			double koef;
+
+
+			string res="";
+			foreach (double power in powers) {
+				eqByX = 0;
+				diffByX = 0;
+				idealByX = 0;
+
+				rusa.getMinRashod(allGa, napors, power);
+
+				foreach (double napor in napors){
+					ideal = 1000 * power / (9.81 * napor);
+					Console.Write(String.Format("{0,-3} {1,-3}", napor, power));
+					eq = VotGES.Rashod.RUSA.getOptimRashod(power, napor, true, sostav);
+					Console.Write(String.Format(" e={0:00000.00} k={1:0.00}", eq, ideal / eq));
+					diff = rusa.minRashod[napor];
+
+					koef = diff / eq;
+
+					Console.WriteLine(String.Format(" d={0:00000.00} k={1:0.00} ({2}={3})", diff, ideal / diff, String.Join("-", sostav), String.Join("-", rusa.minSostav[napor].Values)));
+
+					eqByX += eq;
+					diffByX += diff;
+					idealByX += ideal;
+
+					eqRashod += eq;
+					diffRashod += diff;
+					idealRashod += ideal;
+				}
+			}
+		}
+
+
 		protected static void calc(List<double>X, List<double> Y, bool isFirstPower, string fn) {
+			List<int> allGa=new List<int>();
+			for(int ga=1;ga<=10;ga++)
+				allGa.Add(ga);
 			List<int> sostav=new List<int>();
 			RUSADiffPower rusa=new RUSADiffPower();
 			double eqRashod=0;
@@ -73,7 +132,7 @@ namespace RUSA
 					Console.Write(String.Format("{0,-3} {1,-3}", napor, power));
 					eq = VotGES.Rashod.RUSA.getOptimRashod(power, napor, true, sostav);
 					Console.Write(String.Format(" e={0:00000.00} k={1:0.00}", eq, ideal/eq));
-					diff = rusa.getMinRashod(sostav, power, napor); 					
+					diff = rusa.getMinRashod(allGa, power, napor); 					
 					koef=diff / eq;
 
 					Console.WriteLine(String.Format(" d={0:00000.00} k={1:0.00} ({2}={3})", diff, ideal/diff, String.Join("-", sostav), String.Join("-", rusa.minSostav.Values)));
---
+++ VotGES/Rashod/RUSADiffPowerNapors.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VotGES.Rashod
+{
+	public class RUSADiffPowerNapors
+	{
+		public List<int> availGenerators;
+
+		public SortedList<int,double> currentSostav;
+		public SortedList<double,SortedList<int,double>> minSostav;
+		public SortedList<double,double> minRashod;
+
+		public double needPower;
+		public List<double> napors;
+
+		public RUSADiffPowerNapors() {
+			availGenerators=new List<int>();
+			
+		}
+
+		public void getMinRashod(List<int> avail, List<double> napors,double power) {
+			this.needPower = power;
+			this.napors = napors;
+			minRashod = new SortedList<double, double>();
+			minSostav = new SortedList<double, SortedList<int, double>>();
+			currentSostav = new SortedList<int, double>();
+			foreach (double napor in napors) {
+				minRashod.Add(napor, 10e8);
+				minSostav.Add(napor, new SortedList<int, double>());				
+			}
+			availGenerators = avail;
+			currentSostav.Clear();
+			foreach (double napor in napors) {
+				minSostav[napor].Clear();
+			}
+
+			int index=0;
+			foreach (int ga in avail) {
+				foreach (double napor in napors) {
+					minSostav[napor].Add(ga, 0);
+				}
+				currentSostav.Add(ga, 0);
+				index++;
+			}
+
+			
+
+			calc(0);
+			//Logger.Info(String.Join("~", minSostav.Values));
+			//return minRashod;
+		}
+
+		public void calc(int gaIndex) {			
+			int gaNumber=availGenerators[gaIndex];
+			int nextGACount=availGenerators.Count - (gaIndex + 1);			
+			double sumPower=0;
+			SortedList<double,double> sumRashod=new SortedList<double, double>();
+			foreach (double napor in napors) {
+				sumRashod[napor] = 0;
+			}
+			double gaRashod;
+
+			for (int i=0; i < gaIndex; i++) {
+				int ga=availGenerators[i];
+				sumPower += currentSostav[ga];
+				foreach (double napor in napors) {
+					sumRashod[napor] += currentSostav[ga] > 0 ? RashodTable.getRashod(ga, currentSostav[ga], napor) : 0;
+				}
+			}
+			if (sumPower + (nextGACount+1) * 100 < needPower)
+				return;
+			if (gaIndex < availGenerators.Count) {
+				for (int power=0; power <= 100; power+=1) {
+					if (sumPower + power > needPower)
+						break;
+					if ((sumPower+power + 100 * nextGACount < needPower)|| (power != 0 && power < 35))
+						continue;
+					foreach (double napor in napors) {
+						gaRashod = RashodTable.getRashod(gaNumber, power, napor);
+						currentSostav[gaNumber] = power;
+						//Logger.Info(String.Join("~", currentSostav.Values));
+						if ((sumPower + power == needPower)) {
+							if (sumRashod[napor] + gaRashod < minRashod[napor]) {
+								minRashod[napor] = sumRashod[napor] + gaRashod;
+								foreach (int ga in availGenerators) {
+									minSostav[napor][ga] = currentSostav[ga];
+								}
+							}
+						}
+					}
+					if (gaIndex < availGenerators.Count - 1) {
+						calc(gaIndex + 1);
+					}
+				}
+				currentSostav[gaNumber] = 0;
+			}
+		}	
+
+		
+	}
+}
